f08c8f5bc20b1730f4f958af9eaab8e6077097ed
deepanshu <21f3001217@ds.study.iitm.ac.in>
2025-02-03 18:15:12 +0530

    first commit

---
+++ health-tracker/src/app/components/add-workout/add-workout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-workout',
+  imports: [],
+  templateUrl: './add-workout.component.html',
+  styleUrl: './add-workout.component.scss'
+})
+export class AddWorkoutComponent {
+
+}
